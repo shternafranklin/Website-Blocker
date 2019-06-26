4c8c2c65987397d754402e124e2ca2f852429c3d
Sara Franklin <shternafranklin@gmail.com>
2019-06-26 06:38:08 -0400

    Update web_blocker.py

--- web_blocker.py
+++ web_blocker.py
@@ -15,8 +15,25 @@ website_list =["www.facebook.com", "facebook.com", "www.youtube.com", "youtube.c
 #use variables in dt.now so its always true
 
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 16) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
         print("Working Hours.. ")
-    else:
-        print("Fun Hours")
+        with open(hosts_path, 'r+' ) as file: #r+ can read/write
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass #dont want to do anything, so just pass
+                else:
+                    file.write("\t"+ redirect+" "+website+"\n")
+    else: #now we need to delete those lines
+        with open(hosts_path, 'r+' ) as file:
+            content = file.readlines() #make it a list to check it against the list
+            #When you are appending the pointer right in the beginning and replace from the beg
+            file.seek(0)
+            #no way to actually delete, just append them
+            for line in content:
+                #if any item in each line of the file is not in the website list
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate() #removes everything after the seek method
+        print("Fun Hours...")
     time.sleep(5)
